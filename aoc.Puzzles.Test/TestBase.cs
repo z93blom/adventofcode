aabd1e8f8129174f06056c4a07fafa6f0c27524f
Fredrik Blom <fredrik.blom@kollmorgen.com>
2019-12-17 14:03:39 +0100

    Further work on getting the new framework up and running.

---
+++ aoc.Puzzles.Test/TestBase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Channels;
+using aoc.Puzzles.Core;
+
+namespace aoc.Puzzles.Test
+{
+    public abstract class TestBase<TSolution> where TSolution : ISolution
+    {
+        private readonly Channel<string> _channel;
+        protected ChannelWriter<string> Writer => _channel.Writer;
+        protected TSolution Solution { get; }
+
+        protected TestBase()
+        {
+            Solution = Activator.CreateInstance<TSolution>();
+            _channel = Channel.CreateUnbounded<string>();
+        }
+
+
+    }
+}
